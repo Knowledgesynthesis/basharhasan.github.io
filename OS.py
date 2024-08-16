3a8b59c9b98ed55a1634a59c79e0aee8de275003
Bashar Hasan <79866933+Knowledgesynthesis@users.noreply.github.com>
2024-08-16 17:50:26 -0500

    Update OS.py

--- OS.py
+++ OS.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-# Probability data for risk scores (directly added from the CSV)
+# Probability data for risk scores
 data = {
     'Risk Score': [-27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
     'Predicted 3-Year Probability of Death (%)': [0.072, 0.085, 0.101, 0.119, 0.141, 0.166, 0.197, 0.233, 0.275, 0.326, 0.385, 0.455, 0.539, 0.637, 0.752, 0.889, 1.05, 1.24, 1.464, 1.728, 2.039, 2.403, 2.831, 3.333, 3.92, 4.605, 5.404, 6.331, 7.406, 8.646, 10.071, 11.701, 13.555, 15.65, 18.002, 20.622, 23.512, 26.673, 30.09, 33.744, 37.603, 41.627, 45.765, 49.962, 54.16, 58.299, 62.325, 66.187, 69.845, 73.268, 76.433, 79.328, 81.952, 84.309, 86.409, 88.268, 89.901, 91.33, 92.573, 93.651, 94.581, 95.381, 96.069, 96.657, 97.16, 97.59, 97.955],
@@ -29,6 +29,18 @@ def calculate_risk_score(who_grade, tstage, cirrhosis, portal_hyp, hepar, gpc, r
     
     score += -3 * round(int(r_rpa) / 10)
     return score
+
+# Define the function to get the associated risk probabilities
+def get_risk_probabilities(score, prob_data):
+    if score in prob_data['Risk Score'].values:
+        risk_3yr = prob_data[prob_data['Risk Score'] == score]['Predicted 3-Year Probability of Death (%)'].values[0]
+        risk_5yr = prob_data[prob_data['Risk Score'] == score]['Predicted 5-Year Probability of Death (%)'].values[0]
+        survival_3yr = 100 - risk_3yr
+        survival_5yr = 100 - risk_5yr
+        return risk_3yr, risk_5yr, survival_3yr, survival_5yr
+    else:
+        return "Score out of range.", "Score out of range.", "Score out of range.", "Score out of range."
+
 
 # Streamlit app
 st.title("HCC Overall Survival Probability Calculator")
